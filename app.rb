409d00b0db20fc50cb5c736d6813e7788dbf9fd0
Niall Wallace <nj.wallace@outlook.com>
2015-09-02 17:47:22 +0100

    compleated basic functionality

--- app.rb
+++ app.rb
@@ -1,8 +1,23 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'pry'
+require_relative './models/translate'
+
 
 get '/' do
-  @title = Home
-  erb :translate
+  @title = 'Pig Latin Generator'
+  erb :home
 end
+
+
+post '/translate' do
+  @title = 'Pig Latin Generator'
+  @sample_text = params[:text]
+  @pig_text = Latinize.pig @sample_text
+
+ erb :home
+
+end
+
+
+
